d1e099b5fe7686b8dcb1242da45f9c63d322b14c
Ernst Bolt <er.bolt@windesheim.nl>
2023-03-17 15:32:32 +0100

    oefenopgave javascript modules

---
+++ lessen/week-1/les-1/oefenopgave-javascript-modules/studentversie/app.js
@@ -0,0 +1,8 @@
+
+
+//student uitwerking
+
+let logger = new Log();
+logger.info('info');
+logger.error('error');
+logger.debug('debug');
---
+++ lessen/week-1/les-1/oefenopgave-javascript-modules/studentversie/log.js
@@ -0,0 +1,19 @@
+
+
+//student uitwerking
+//class Log {
+
+    info(message) {
+        console.info(message);
+    }
+
+    debug(message) {
+        console.log(message);
+    }
+
+    error(message) {
+        console.error(message);
+    }
+
+}
+
