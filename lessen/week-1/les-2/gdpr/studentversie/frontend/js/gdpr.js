c7e29c20630c12ce50006a9401f5cc93e3f29bec
ernstbolt <er.bolt@windesheim.nl>
2024-02-01 15:15:02 +0100

    Update gdpr.js

--- lessen/week-1/les-2/gdpr/studentversie/frontend/js/gdpr.js
+++ lessen/week-1/les-2/gdpr/studentversie/frontend/js/gdpr.js
@@ -71,23 +71,6 @@ class GDPR {
         document.querySelector(`.gdpr-consent`).classList.add('show');
     }
 
-    // https://www.w3schools.com/js/js_cookies.asp
-    getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
 }
 
 const gdpr = new GDPR();
